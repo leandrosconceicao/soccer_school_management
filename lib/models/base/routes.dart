4eedd4c09ee690f7dbcbeb59f69351f3b8e28464
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-09-14 18:06:58 -0300

    Model de rotas

---
+++ lib/models/base/routes.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Routes {
+
+  String routeName;
+  String title;
+  Icon? icon;
+
+  Routes.classes() : routeName = "/turmas", title = "Turmas", icon = const Icon(Icons.group);
+  // Routes.cardLog() : routeName = cardLogRoute, title = "Card log", icon = const Icon(Icons.credit_card);
+
+  // Routes.devolution() : routeName = devolutionRoute, title = "Devolução", icon = const Icon(Icons.production_quantity_limits);
+
+  // Routes.home() : routeName = homeRoute, title = "Home", icon = const Icon(Icons.home);
+
+  // Routes.unknown() : routeName = unknownRoute, title = "Página não encontrada";
+
+  // Routes.splash() : routeName = splashRoute, title = "Loading";
+
+  // Routes.monitor() : routeName = monitorRoute, title = "Consultas", icon = const Icon(Icons.monitor);
+
+  // Routes.creditTransfer() : routeName = transferCreditRoute, title = "Solic. Transf. de Crédito", icon = const Icon(Icons.sync_alt);
+
+  // static const devolutionRoute = "/devolution";
+  // static const homeRoute = "/";
+  // static const unknownRoute = "/pagina-nao-encontrada";
+  // static const splashRoute = "/loading";
+  // static const monitorRoute = "/consults";
+  // static const transferCreditRoute = "/solic_credit_transf";
+  // static const cardLogRoute = "/cardlog";
+}
