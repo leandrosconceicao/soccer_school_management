e7691bd88960cabda27e4025d17ba333ff59b982
leandrosconceicao <leandrosantosconceicao@gmail.com>
2023-09-14 18:07:16 -0300

    Refatorações

---
+++ lib/views/pages/classes/class_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class ClassesHome extends StatefulWidget {
+  const ClassesHome({super.key});
+
+  @override
+  State<ClassesHome> createState() => _ClassesHomeState();
+}
+
+class _ClassesHomeState extends State<ClassesHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
